b663549bc00ada54fabfc029d1b19da96dcb5454
David Duarte <jesusdavidduartejimenez@gmail.com>
2021-11-23 16:58:56 -0500

    context errors

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
+import React, { Component } from 'react';
+
 import classes from './User.module.css';
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  componentWillUnmount() {
+    console.log('user will unmount');
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
 
 export default User;
